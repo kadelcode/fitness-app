39d159ad893239336dbc0910ed33c70ef2a2c0e4
kadelcode <kadelcode@gmail.com>
2025-04-17 14:45:43 +0100

    Add testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/sections/Hero";
 import Features from "./components/sections/Features";
+import Testimonials from "./components/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
+      <Testimonials />
     </>
   )
 }
