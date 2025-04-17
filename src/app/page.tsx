6a7b56f69d11bd625ac7b8bb62bc1912320926c6
kadelcode <kadelcode@gmail.com>
2025-04-17 18:20:18 +0100

    FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/sections/Hero";
 import Features from "./components/sections/Features";
 import Pricing from "./components/sections/Pricing";
 import Testimonials from "./components/sections/Testimonials";
+import FAQ from "./components/sections/FAQ";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Features />
       <Pricing />
       <Testimonials />
+      <FAQ />
     </>
   )
 }
