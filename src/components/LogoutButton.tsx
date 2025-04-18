0eb1b1ec81dcf94d414fe15ff74629dfeb41670d
kadelcode <kadelcode@gmail.com>
2025-04-18 21:23:00 +0100

    Update code

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -10,6 +10,7 @@ export default function LogoutButton() {
             await signOut({ callbackUrl: "/login" });
             toast.success("Logged out successfully!");
         } catch (error) {
+            console.log(error);
             toast.error("Failed to log out.");
         }
     };
