61f3aadd7d7b18112f9692c472ca11abd6a52968
kadelcode <kadelcode@gmail.com>
2025-04-19 08:07:30 +0100

    Add next-auth types

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,19 @@
+// types/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
