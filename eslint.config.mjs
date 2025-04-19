480c9e2aa1182a0ab72e8e381840b710fdc3b60b
kadelcode <kadelcode@gmail.com>
2025-04-19 09:10:53 +0100

    Suppress eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,14 +12,23 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  // Over automatically generated prisma folder
+  // Suppress rules for all generated Prisma JS files
   {
     files: ["src/generated/prisma/**/*.js"],
     rules: {
       "@typescript-eslint/no-require-imports": "off",
-      "no-unused-vars": "off", // Suppresses "assigned a value but never used"
+      "@typescript-eslint/no-unused-vars": "off", // Suppresses "assigned a value but never used"
+      "@typescript-eslint/no-unused-expressions": "off",
     },
-  }
+  },
+
+  // Suppress "unused vars" for type-only files
+  {
+    files: ["src/types/**/*.d.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
