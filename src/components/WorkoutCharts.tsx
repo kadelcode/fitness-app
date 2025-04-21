d1e43d3eb6221359b2fd3b90fa805e40d2c4844c
kadelcode <kadelcode@gmail.com>
2025-04-21 17:28:07 +0100

    Add new components

---
+++ src/components/WorkoutCharts.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
+const workoutData = [
+    { day: 'Mon', workouts: 1 },
+    { day: 'Tue', workouts: 0 },
+    { day: 'Wed', workouts: 1 },
+    { day: 'Thu', workouts: 0 },
+    { day: 'Fri', workouts: 1 },
+  ];
+
+
+
+export default function WorkoutCharts() {
+    return (
+        <Card>
+            <CardHeader>Weekly Workout Overview</CardHeader>
+            <CardContent>
+                <ResponsiveContainer width="100%" height={200}>
+                    <BarChart data={workoutData}>
+                        <XAxis dataKey="day" />
+                        <YAxis allowDecimals={false} />
+                        <Tooltip />
+                        <Bar dataKey="workouts" fill="#4f46e5" radius={[4, 4, 0, 0]} />
+                    </BarChart>
+                </ResponsiveContainer>
+            </CardContent>
+        </Card>
+    )
+}
